44797ab93543be792c457e020828af8d2b7d12e7
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-03-12 23:29:10 -0500

    Elements and Bitcoin

--- Exercises/Bitcoin.java
+++ Exercises/Bitcoin.java
@@ -1,8 +1,44 @@
 package Exercises;
 import Data.*;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class Bitcoin {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> values = new ArrayList<Integer>();
+
+        values = (ArrayList<Integer>) Stream.of(input.nextLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        DynamicList precios = new DynamicList();
+
+        for (int value : values){
+            precios.pushBack(value);
+        }
+
+        StaticList dias = new StaticList(precios.index);
+
+        for (int i=0; i< precios.index; i++) {
+            int diasEspera = 0;
+            for (int j=i+1; j< precios.index; j++){
+                if (precios.list[j]>precios.list[i]){
+                    diasEspera = j-i;
+                    j = precios.index;
+                }
+            }
+            dias.pushBack(diasEspera);
+        }
+
+        System.out.println(dias);
+
+        input.close();
     }
 }
