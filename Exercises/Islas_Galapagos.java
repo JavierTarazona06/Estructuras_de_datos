3e96798f1ac744d8f0ba78d38448c34b93926e9b
Javier Tarazona <96489034+JavierTarazona06@users.noreply.github.com>
2023-05-14 23:34:25 -0500

    Update

--- Exercises/Islas_Galapagos.java
+++ Exercises/Islas_Galapagos.java
@@ -1,4 +1,18 @@
 package Exercises;
+import Data.*;
+
+import java.util.Scanner;
 
 public class Islas_Galapagos {
+    public static void main(String[] args) throws Exception {
+        AVL<Integer> isla = new AVL<Integer>();
+        Scanner input = new Scanner(System.in);
+
+        //String data = input.nextLine();
+        String data =  "4 20 10 -1 16 4 0";
+        isla.lineTOInsertRep(data);
+
+        System.out.println(isla.levelOrder());
+        System.out.println(isla.inOrder());
+    }
 }
