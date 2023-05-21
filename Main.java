5e95c9397685a00aedee9585be2f386f8b0e3d06
JavierTarazona06 <96489034+JavierTarazona06@users.noreply.github.com>
2023-05-21 15:06:22 +0000

    Replit Change

--- Main.java
+++ Main.java
@@ -1,52 +1,58 @@
+import Data.*;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        //LinkedList.main();
-        //OrderedLinkedList.main();
-        //StaticList.main();
-        /*Scanner input = new Scanner(System.in);
-
-        boolean exitFlag = true;
-
-        while(exitFlag){
-
-            boolean badOptionFlag = true;
-
-            while(badOptionFlag){
-                System.out.println("Bienvenido al Programa de Estructuras de Datos \n"+
-                        "Elige la estructura de datos a trabajar:");
-
-                System.out.println("1. Static List");
-
-                try {
-                    int option =Integer.parseInt(input.nextLine());
-
-                    if (option==1){
-                        badOptionFlag = false;
-                        //StaticList.main();
-                    } else {
-                        System.out.println("Number not valid");
-                    }
-                } catch (Exception e) {
-                    System.out.println("Data not valid");
-                }
-            }
-
-
-            System.out.println("Se ha terminado la sesión");
-
-            System.out.println("¿Desea salir? \n" +
-                    "1. Sí \n" +
-                    "2. No");
-
-            int exit = Integer.parseInt(input.nextLine());
-            //int exit = 0;
-
-            if (exit==1){
-                exitFlag = false;
-            }
-        }
-
-        input.close();
-        */
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello");
+    HeapMin myHeap = new HeapMin();
+    myHeap.main();
+    // LinkedList.main();
+    // OrderedLinkedList.main();
+    // StaticList.main();
+    /*
+     * Scanner input = new Scanner(System.in);
+     * 
+     * boolean exitFlag = true;
+     * 
+     * while(exitFlag){
+     * 
+     * boolean badOptionFlag = true;
+     * 
+     * while(badOptionFlag){
+     * System.out.println("Bienvenido al Programa de Estructuras de Datos \n"+
+     * "Elige la estructura de datos a trabajar:");
+     * 
+     * System.out.println("1. Static List");
+     * 
+     * try {
+     * int option =Integer.parseInt(input.nextLine());
+     * 
+     * if (option==1){
+     * badOptionFlag = false;
+     * //StaticList.main();
+     * } else {
+     * System.out.println("Number not valid");
+     * }
+     * } catch (Exception e) {
+     * System.out.println("Data not valid");
+     * }
+     * }
+     * 
+     * 
+     * System.out.println("Se ha terminado la sesión");
+     * 
+     * System.out.println("¿Desea salir? \n" +
+     * "1. Sí \n" +
+     * "2. No");
+     * 
+     * int exit = Integer.parseInt(input.nextLine());
+     * //int exit = 0;
+     * 
+     * if (exit==1){
+     * exitFlag = false;
+     * }
+     * }
+     * 
+     * input.close();
+     */
+  }
 }
